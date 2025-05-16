4a38191d6bdba925db9986be46f0b44f3c408dc7
Jaiderpe <160280115+Jaiderpe@users.noreply.github.com.>
2025-05-16 16:11:17 -0500

    feat: :sparkles: pom file organization,tools categories

--- api/src/main/java/com/herramientas/api/domain/entities/fkClass/HerramientaCategoriaId.java
+++ api/src/main/java/com/herramientas/api/domain/entities/fkClass/HerramientaCategoriaId.java
@@ -1,13 +1,11 @@
 package com.herramientas.api.domain.entities.fkClass;
 
 import java.io.Serializable;
-
 import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class HerramientaCategoriaId implements Serializable {
     private Integer idHerramienta;
     private Integer idCategoria;
